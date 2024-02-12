f1f00a48634d8bb186b3c2ef9a6b706fbc67a159
David Perelman <dperelman3@gmail.com>
2024-02-12 10:39:47 +0200

    split final table to 2 equals\not equals tables

--- py_scripts/merge_all_data.py
+++ py_scripts/merge_all_data.py
@@ -5,4 +5,4 @@ def merge_all_data():
     jtmt_scenario = pd.read_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\scenarios_by_first_year\scenario_by_first_year_jtmt_scenario.xlsx')
     trend_scenario = pd.read_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\scenarios_by_first_year\scenario_by_first_year_trend_scenario.xlsx')
 
-    housing_scenario.merge(jtmt_scenario, how="outer").merge(trend_scenario, how="outer").to_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\find_year_for_new_neighborhood_by_scenario.xlsx', index=False)
+    housing_scenario.merge(jtmt_scenario, how="outer").fillna(0).merge(trend_scenario, how="outer").fillna(0).to_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\find_year_for_new_neighborhood_by_scenario.xlsx', index=False)
---
+++ py_scripts/split_final_table.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def split_final_table():
+    find_year_for_new_neighborhood_by_scenario = pd.read_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\find_year_for_new_neighborhood_by_scenario.xlsx')
+
+    equal_data = []
+    not_equal_data = []
+
+   # Iterate through each row in the DataFrame
+    for index, row in find_year_for_new_neighborhood_by_scenario.iterrows():
+        Taz_num = row[0]
+
+        # Initialize a flag to track if all elements in the row are equal
+        all_equal = True
+
+         # Iterate through each element in the row starting from the second element
+        for i in range(2, len(row)):
+            # Check if the current element is equal to the previous element
+            if row[i] != row[i - 1]:
+                all_equal = False
+                break
+        
+        # Print the result for the current row
+        if all_equal:
+            data_row = {'Taz_num': Taz_num, 'year': row[2]}
+            equal_data.append(data_row)
+            print("All elements in the row are equal")
+        else:
+            # data_row = {'Taz_num': Taz_num, 'year': row[2]}
+            not_equal_data.append(row)
+            print("Not all elements in the row are equal")
+
+    new_equal = pd.DataFrame(equal_data)
+    new_not_equal = pd.DataFrame(not_equal_data)
+    new_equal.to_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\scenarios_by_first_year\scenario_by_first_year_equal.xlsx', index=False)
+    new_not_equal.to_excel(r'C:\Users\dpere\Documents\JTMT\find_year_for_new_neighborhood_by_scenario\scenarios_by_first_year\scenario_by_first_year_not_equal.xlsx', index=False)
